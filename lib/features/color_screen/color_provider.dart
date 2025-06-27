d73cef8203a7fb68664fb4b60043780b27ec2814
tricksterd <kozavkolena@gmail.com>
2025-06-27 20:39:33 +0300

    changed formatting

--- lib/features/color_screen/color_provider.dart
+++ lib/features/color_screen/color_provider.dart
@@ -4,5 +4,5 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:random_color_generator/features/color_screen/color_notifier.dart';
 
 final colorNotifierProvider = NotifierProvider<ColorNotifier, Color>(
-  ColorNotifier.new
+  ColorNotifier.new,
 );
